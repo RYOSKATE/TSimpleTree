7f2b347739763d61384ef0bf6cc8d228fccc4732
RYOSUKE ISHIZUE <ryoskate@gmail.com>
2018-01-16 22:29:14 +0900

    add Create2Nodes

--- test/NodeTest.ts
+++ test/NodeTest.ts
@@ -3,14 +3,20 @@ import { assert } from 'chai';
 import { StringNode } from '../src/index';
 
 // npm run test
-describe('CreateN-Node', () => {
-  const node = new StringNode('a');
-  it('Create1Node1', () => {
+describe('CreateN-Nodes', () => {
+  it('Create1Node', () => {
+    const node = new StringNode('a');
     const actual = node.toString().normalizeNewLine();
     assert.equal(actual, 'a\n');
+    const actua2 = node.Descendants().select(n => n.Value).toJoinedString('');
+    assert.equal(actua2, '');
   });
-  it('Create1Node2', () => {
-    const actual = node.Descendants().select(n => n.Value).toJoinedString('');
-    assert.equal(actual, '');
+  it('Create2Nodes', () => {
+    const node = new StringNode('a');
+    node.AddFirst(new StringNode('b'));
+    const actual = node.toString().normalizeNewLine();
+    assert.equal(actual, 'a\n  b\n');
+    const actua2 = node.Descendants().select(n => n.Value).toJoinedString('');
+    assert.equal(actua2, 'b');
   });
 });
