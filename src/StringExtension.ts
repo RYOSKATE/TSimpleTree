d439659eb059c3966b960c9c76cafeafd551077c
RYOSUKE ISHIZUE <ryoskate@gmail.com>
2018-01-17 08:01:34 +0900

    fix StringExtensions

--- src/StringExtension.ts
+++ src/StringExtension.ts
@@ -1,19 +1,8 @@
 // インターフェイス統合によるコアクラスの拡張
 declare interface String {
-  /**
-   文字列を挿入する
-    @param insertIdx 挿入する位置
-    @param insertString 挿入する文字列
-  */
-  insert(insertIdxdx: number, insertString: string): string;
-
   normalizeNewLine(): string;
 }
 
-String.prototype.insert = function (insertIdx: number, insertString: string) {
-  return (this.slice(0, insertIdx) + insertString + this.slice(insertIdx));
-};
-
 String.prototype.normalizeNewLine = function () {
-  return this.replace(/\r?\n/g, '\n');
+  return this.replace(/\r?\n/g, '\r\n');
 };
--- test/NodeTest.ts
+++ test/NodeTest.ts
@@ -6,16 +6,16 @@ import { StringNode } from '../src/index';
 describe('CreateN-Nodes', () => {
   it('Create1Node', () => {
     const node = new StringNode('a');
-    const actual = node.toString().normalizeNewLine();
-    assert.equal(actual, 'a\n');
+    const actual = node.toString();
+    assert.equal(actual, 'a\n'.normalizeNewLine());
     const actua2 = node.Descendants().select(n => n.Value).toJoinedString('');
     assert.equal(actua2, '');
   });
   it('Create2Nodes', () => {
     const node = new StringNode('a');
     node.AddFirst(new StringNode('b'));
-    const actual = node.toString().normalizeNewLine();
-    assert.equal(actual, 'a\n  b\n');
+    const actual = node.toString();
+    assert.equal(actual, 'a\n  b\n'.normalizeNewLine());
     const actua2 = node.Descendants().select(n => n.Value).toJoinedString('');
     assert.equal(actua2, 'b');
   });
@@ -23,8 +23,8 @@ describe('CreateN-Nodes', () => {
     const node = new StringNode('a');
     node.AddLast(new StringNode('b'));
     node.AddFirst(new StringNode('c'));
-    const actual = node.toString().normalizeNewLine();
-    assert.equal(actual, 'a\n  c\n  b\n');
+    const actual = node.toString();
+    assert.equal(actual, 'a\n  c\n  b\n'.normalizeNewLine());
     const actua2 = node.Descendants().select(n => n.Value).toJoinedString('');
     assert.equal(actua2, 'cb');
   });
@@ -33,8 +33,8 @@ describe('CreateN-Nodes', () => {
     node.AddLast(new StringNode('b'));
     node.AddFirst(new StringNode('c'));
     node.AddLast(new StringNode('d'));
-    const actual = node.toString().normalizeNewLine();
-    assert.equal(actual, 'a\n  c\n  b\n  d\n');
+    const actual = node.toString();
+    assert.equal(actual, 'a\n  c\n  b\n  d\n'.normalizeNewLine());
     const actua2 = node.Descendants().select(n => n.Value).toJoinedString('');
     assert.equal(actua2, 'cbd');
   });
