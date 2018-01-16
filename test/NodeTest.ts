49dcb678fa014e126bf1ccddafde630a680ddafd
RYOSUKE ISHIZUE <ryoskate@gmail.com>
2018-01-17 08:15:12 +0900

    TraverseSingles

--- test/NodeTest.ts
+++ test/NodeTest.ts
@@ -49,4 +49,35 @@ describe('StringNodeTest', () => {
     const actual = a.DescendantsAndSelf().select(n => n.Value).toJoinedString('');
     assert.equal(actual, 'ad');
   });
+  it('TraverseSingles', () => {
+    const a = new StringNode('a');
+    const b = new StringNode('b');
+    const c = new StringNode('c');
+    const d = new StringNode('d');
+    const e = new StringNode('e');
+    const f = new StringNode('f');
+    const g = new StringNode('g');
+    // a - b - c - d - e
+    //   - g         - f
+    a.AddFirst(b);
+    a.AddLast(g);
+    b.AddFirst(c);
+    c.AddFirst(d);
+    d.AddFirst(e);
+    d.AddLast(f);
+
+    assert.equal(b.DescendantsOfSingle().select(n => n.Value).toJoinedString(''),'cd');
+    assert.equal(b.DescendantsOfSingleAndSelf().select(n => n.Value).toJoinedString(''),'bcd');
+    assert.equal(c.DescendantsOfSingle().select(n => n.Value).toJoinedString(''),'d');
+    assert.equal(c.DescendantsOfSingleAndSelf().select(n => n.Value).toJoinedString(''),'cd');
+
+    assert.equal(b.AncestorsWithSingleChild().select(n => n.Value).toJoinedString(''),'');
+    assert.equal(b.AncestorsWithSingleChildAndSelf().select(n => n.Value).toJoinedString(''),'b');
+    assert.equal(c.AncestorsWithSingleChild().select(n => n.Value).toJoinedString(''),'b');
+    assert.equal(c.AncestorsWithSingleChildAndSelf().select(n => n.Value).toJoinedString(''),'cb');
+    assert.equal(d.AncestorsWithSingleChild().select(n => n.Value).toJoinedString(''),'cb');
+    assert.equal(d.AncestorsWithSingleChildAndSelf().select(n => n.Value).toJoinedString(''),'dcb');
+    assert.equal(e.AncestorsWithSingleChild().select(n => n.Value).toJoinedString(''),'');
+    assert.equal(e.AncestorsWithSingleChildAndSelf().select(n => n.Value).toJoinedString(''),'e');
+  });
 });
